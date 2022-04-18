613e1a5c13dd6f38158d8279016757d7630d7ded
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-04-18 03:13:51 -0300

    Criando botões de Tipo

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,31 +1,55 @@
 import Modal from 'react-modal';
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg'
-import { Container } from './styles';
+import { Container, TransationTypeContainer } from './styles';
 
-interface NewTransactionModalProps{
+interface NewTransactionModalProps {
     isOpen: boolean;
-    onRequestClose:()=>void;
-    
+    onRequestClose: () => void;
+
 }
 
-export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps){
+export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
     return (
-        <Modal 
-                isOpen={isOpen} 
-                onRequestClose={onRequestClose}
-                overlayClassName="react-modal-overlay"
-                className='react-modal-content'
-                >
-                    <button type="button" onClick={onRequestClose} className="react-modal-close">
-                        <img src={closeImg} alt="Fechar modal" />
-                    </button>
-                <Container>
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className='react-modal-content'
+        >
+            <button type="button" onClick={onRequestClose} className="react-modal-close">
+                <img src={closeImg} alt="Fechar modal" />
+            </button>
+            <Container>
                 <h2>Cadastrar transação</h2>
-                <input placeholder='Título' />
-                <input type="number" placeholder='Valor' />
-                <input  placeholder='Categoria' />
-                <button type="submit">Cadastrar</button>
-                </Container>
-            </Modal>
+                <input
+                    placeholder='Título' />
+
+                <input
+                    type="number"
+                    placeholder='Valor' />
+
+                <TransationTypeContainer>
+                    <button
+                        type='button'>
+                        <img src={incomeImg} alt="Entrada" />
+                        <span>Entrada</span>
+                    </button>
+                    <button
+                        type='button'>
+                        <img src={outcomeImg} alt="Saída" />
+                        <span>Saída</span>
+                    </button>
+                </TransationTypeContainer>
+
+
+                <input placeholder='Categoria' />
+
+                <button type="submit">
+                    Cadastrar
+                </button>
+            </Container>
+        </Modal>
     );
 }
