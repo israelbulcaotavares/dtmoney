9dcb53c40aea9c4318da640beb51a3672283bd5d
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-04-19 02:00:51 -0300

    Listando transações e seeds

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,45 +1,49 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
- 
 
-export function TransactionsTable(){
+interface Transaction{
+    id:number;
+    title:string;
+    amount:string;
+    type:string;
+    category:string;
+    createdAt:string;
+}
+
+export function TransactionsTable() {
+
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+
     useEffect(() => {
-         api.get('/transactions')
-        .then(response => console.log(response.data))
-    },[]);
+        api.get('transactions')
+            .then(response => setTransactions(response.data.transactions))
+    }, []);
 
-    return(
+    return (
         <Container>
             <table>
                 <thead>
-                <tr>
-                    <th>Título</th>
-                    <th>Valor</th>
-                    <th>Categoria</th>
-                    <th>Data</th>
-                </tr>
-                </thead>    
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
 
                 <tbody>
-                    <tr>
-                        <td>Desenvolvimento de website</td>
-                        <td className="deposit">R$12.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Aluguel</td>
-                        <td className="withdraw">- R$1.100</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Desenvolvimento de website</td>
-                        <td>R$12.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
+                    {transactions.map(transactions =>  (
+                   
+                            <tr key={transactions.id}>
+                                <td>{transactions.title}</td>
+                                <td className={transactions.type}>{transactions.amount}</td>
+                                <td>{transactions.category}</td>
+                                <td>{transactions.createdAt})</td>
+                            </tr>
+                       
+                       ))}
+
                 </tbody>
             </table>
         </Container>
