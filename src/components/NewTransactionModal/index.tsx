3360089ff38cccc0bd5b7c0180e7b3bd398b686c
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-04-20 04:07:21 -0300

    Criando hook

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,8 +3,9 @@ import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg'
 import { Container, TransationTypeContainer, RadioBox } from './styles';
-import { FormEvent, useState, useContext } from 'react'; 
-import { TransactionsContext } from '../../TransactionsContext';
+import { FormEvent, useState  } from 'react'; 
+import { useTransactions } from '../../hooks/useTransactions';
+ 
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -13,7 +14,7 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
-    const { createTransaction } = useContext(TransactionsContext)
+    const { createTransaction } = useTransactions();
 
     const [title, setTitle] = useState('');
     const [amount, setAmount] = useState(0);
